33ea9dcf34caf6648307147adcaff48f91befa7a
Shiyukine <benjbres@gmail.com>
2022-01-20 21:25:08 +0100

    updated listen component

--- ui/windows/listen/listen.js
+++ ui/windows/listen/listen.js
@@ -12,6 +12,23 @@ export default class listenWindow extends HTMLDivElement
         {
             this.shadowRoot.getElementById("listen").ontransitionend = () => { };
             this.shadowRoot.getElementById("listen").style = ""
+            shadow.getElementById("progressBar").style.width = shadow.getElementById("slider").offsetWidth + "px"
+            shadow.getElementById("progressBar").style.position = "absolute"
+            shadow.getElementById("middle").style.setProperty('--value', shadow.getElementById("slider").value);
+        }
+        window.addEventListener("resize", function() {
+            shadow.getElementById("progressBar").style.width = shadow.getElementById("slider").offsetWidth + "px"
+        })
+        this.shadowRoot.getElementById("slider").oninput = function() {
+            shadow.getElementById("middle").style.setProperty('--value', shadow.getElementById("slider").value);
+        }
+        this.shadowRoot.getElementById("pbInfo").onmouseenter = function() {
+            shadow.getElementById("progressBar").style.visibility = "hidden"
+            shadow.getElementById("slider").style.visibility = "visible"
+        }
+        this.shadowRoot.getElementById("pbInfo").onmouseleave = function() {
+            shadow.getElementById("progressBar").style.visibility = "visible"
+            shadow.getElementById("slider").style.visibility = "hidden"
         }
     }
 }
