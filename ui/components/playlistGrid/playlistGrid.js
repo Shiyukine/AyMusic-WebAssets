7b05469551b524bdd672a5ad6af68bcce6d6b812
Shiyukine <benjbres@gmail.com>
2022-03-11 11:07:38 +0100

    preparing component and home

--- ui/components/playlistGrid/playlistGrid.js
+++ ui/components/playlistGrid/playlistGrid.js
@@ -2,27 +2,17 @@ import Import from "../../../class/import.js";
 import Translations from "../../../class/translations.js";
 import Utils from "../../../class/utils/utils.js";
 
-export default class HomeWindow extends HTMLDivElement {
+export default class PlaylistGrid extends HTMLDivElement {
 
     constructor() {
         super();
         var shadow = this.attachShadow({ mode: "open" })
         this.style.opacity = "0%"
         this.style.transition = "opacity 0.7s"
-        Import.getData("/ui/windows/home/home.html").then((html) => {
+        Import.getData("/ui/components/playlistGrid/playlistGrid.html").then((html) => {
             shadow.innerHTML = html
             new Translations(shadow.children[1])
             this.style.opacity = "1"
-            this.refreshHome()
         })
-    }
-
-    async refreshHome() {
-        console.log("Refreshing home")
-        var pls = Utils.libManager.userPlaylists;
-        console.log(pls)
-        for (let pl in pls) {
-
-        }
     }
 }
